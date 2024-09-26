8b0d38b43590b443b6184d629d43abda35de757f
James Boyle <98388371+James-P-Boyle@users.noreply.github.com>
2024-09-26 15:11:50 +0200

    Bug fix

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -62,11 +62,11 @@ class PostResource extends Resource
                     DateTimePicker::make('published_at')
                         ->nullable(),
                     Checkbox::make('featured'),
-                    Select::make('author')
+                    Select::make('user_id')
                         ->relationship('author', 'name')
                         ->searchable()
                         ->required(),
-                    Select::make('category')
+                    Select::make('categories')
                         ->multiple()
                         ->relationship('categories', 'title')
                         ->searchable()
