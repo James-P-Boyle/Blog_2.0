896c9b70af56ee2e248198dd7d9e777e21d9a43c
James Boyle <98388371+James-P-Boyle@users.noreply.github.com>
2024-10-10 14:59:09 +0200

    Add filament widgets

--- app/Filament/Resources/CommentResource/Pages/ListComments.php
+++ app/Filament/Resources/CommentResource/Pages/ListComments.php
@@ -2,9 +2,10 @@
 
 namespace App\Filament\Resources\CommentResource\Pages;
 
-use App\Filament\Resources\CommentResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\CommentResource;
+use App\Filament\Resources\CommentResource\Widgets\LatestCommentsWidget;
 
 class ListComments extends ListRecords
 {
